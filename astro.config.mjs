54b2c089a66703a911b140abf2335a8a266421f1
Alexpulse <alex.boulanger@pulselife.com>
2026-08-05 13:03:15 +0200

    feat: og img

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://alex-boulanger.dev',
 	i18n: {
 		locales: ['en', 'fr'],
 		defaultLocale: 'en',
